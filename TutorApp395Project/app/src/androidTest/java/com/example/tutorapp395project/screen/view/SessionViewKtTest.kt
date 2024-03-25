f5905c22ef14da30e40eb2cc0af2cac69846df83
cabralr2 <cabralr2@mymacewan.ca>
2024-03-24 20:13:15 -0600

    Create SessionViewKtTest
    Also remove SessionInfoPageStudentKtTest and SessionInfoPageTutorKtTest as they are not needed anymore

--- TutorApp395Project/app/src/androidTest/java/com/example/tutorapp395project/screen/view/SessionViewKtTest.kt
+++ TutorApp395Project/app/src/androidTest/java/com/example/tutorapp395project/screen/view/SessionViewKtTest.kt
@@ -1,13 +1,28 @@
 package com.example.tutorapp395project.screen.view
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class SessionViewKtTest {
 
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Before
     fun setUp() {
+        composeTestRule.setContent {
+            SessionView(
+                time = "3:00-4:00",
+                date = "10/10/2021",
+                subject = "Math",
+                with = "With",
+                person = "John Doe"
+            )
+        }
     }
 
     @After
@@ -15,7 +30,14 @@ class SessionViewKtTest {
     }
 
     @Test
-    fun sessionView() {
+    fun sessionView_TimeDateSubjectExists() {
+        composeTestRule.onNodeWithText("time: 3:00-4:00\ndate: 10/10/2021\nsubject: Math\n\n")
+            .assertExists()
+    }
+
+    @Test
+    fun sessionView_WithPersonExists() {
+        composeTestRule.onNodeWithText("With: John Doe\n").assertExists()
     }
 
     @Test
