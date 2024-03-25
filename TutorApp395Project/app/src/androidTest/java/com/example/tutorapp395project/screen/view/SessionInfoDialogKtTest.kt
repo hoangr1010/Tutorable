c7400234536428c4ed88334fcb4584e22650ab33
cabralr2 <cabralr2@mymacewan.ca>
2024-03-24 20:17:43 -0600

    Create SessionInfoDialogueKtTest

--- TutorApp395Project/app/src/androidTest/java/com/example/tutorapp395project/screen/view/SessionInfoDialogKtTest.kt
+++ TutorApp395Project/app/src/androidTest/java/com/example/tutorapp395project/screen/view/SessionInfoDialogKtTest.kt
@@ -1,20 +1,50 @@
 package com.example.tutorapp395project.screen.view
 
-import org.junit.After
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class SessionInfoDialogKtTest {
 
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Before
     fun setUp() {
-    }
-
-    @After
-    fun tearDown() {
+        composeTestRule.setContent {
+            SessionInfoDialog(
+                sessionId = 123,
+                tutorName = "John Doe",
+                subject = "Math",
+                dateIn = "10/10/2021",
+                timeslot = "3:00-4:00"
+            )
+        }
     }
 
     @Test
-    fun sessionInfoDialog() {
+    fun sessionInfoDialog_sessionIdExists() {
+        composeTestRule.onNodeWithText("123").assertExists()
+    }
+    @Test
+    fun sessionInfoDialog_tutorNameExists() {
+        composeTestRule.onNodeWithText("John Doe").assertExists()
+    }
+
+    @Test
+    fun sessionInfoDialog_subjectExists() {
+        composeTestRule.onNodeWithText("Math").assertExists()
+    }
+
+    @Test
+    fun sessionInfoDialog_dateInExists() {
+        composeTestRule.onNodeWithText("10/10/2021").assertExists()
+    }
+
+    @Test
+    fun sessionInfoDialog_timeslotExists() {
+        composeTestRule.onNodeWithText("3:00-4:00").assertExists()
     }
 }
